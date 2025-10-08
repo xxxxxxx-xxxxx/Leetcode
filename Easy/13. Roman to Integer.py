d74991dd9fae053d9c02f401d55632b35325114a
x <karsten.sveia@icloud.com>
2025-10-09 01:33:40 +0200

    Update 13. Roman to Integer.py

--- Easy/13. Roman to Integer.py
+++ Easy/13. Roman to Integer.py
@@ -17,3 +17,23 @@ class Solution:
             p = v
 
         return t   
+
+
+        
+# A faster solution with better runtime
+
+class Solution:
+    __slots__ = ()
+    def romanToInt(self, s: str) -> int:
+        x = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        get = x.__getitem__
+        total = 0
+        prev = 0
+        for a in reversed(s):
+            b = get(a)
+            if b < prev:
+                total -= b
+            else:
+                total += b
+                prev = b
+        return total
