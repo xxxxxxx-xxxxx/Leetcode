e78b99d1a2b04d7eabdd72e9a0a242c8077337c8
x <karsten.sveia@icloud.com>
2025-10-09 01:36:59 +0200

    Update 1. Two Sum.py

--- Easy/1. Two Sum.py
+++ Easy/1. Two Sum.py
@@ -7,3 +7,17 @@ class Solution:
                 return [index_of[need], i]
             index_of[x] = i
         return []
+
+
+
+# A better solution with a faster runtim
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> list[int]:
+        index_of = {}
+        for i, x in enumerate(nums):
+            need = target - x
+            if need in index_of:
+                return [index_of[need], i]
+            index_of[x] = i
+        return []
