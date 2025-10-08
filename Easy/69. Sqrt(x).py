20a835416d6f9ab28109dca91dae34175e5eae7c
x <karsten.sveia@icloud.com>
2025-10-09 01:36:18 +0200

    Update 69. Sqrt(x).py

--- Easy/69. Sqrt(x).py
+++ Easy/69. Sqrt(x).py
@@ -8,3 +8,19 @@ class Solution:
         return y
 
 #Used the Netwon formula which is = (1/2) * (y+(x/y)) for simplicity
+
+
+# A better solution with a faster runtime
+
+from typing import List
+
+class Solution:
+    __slots__ = ()
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        y = 1 << ((x.bit_length() + 1) // 2)
+        while y * y > x:
+            y = (y + x // y) // 2
+        return y
+
