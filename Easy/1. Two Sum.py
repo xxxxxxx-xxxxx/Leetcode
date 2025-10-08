c4601e7b559f54ac58e8fa726ebc173447453352
x <karsten.sveia@icloud.com>
2025-10-09 01:32:55 +0200

    1. Two Sum.py

--- Easy/1. Two Sum.py
+++ Easy/1. Two Sum.py
@@ -7,3 +7,18 @@ class Solution:
                 return [index_of[need], i]
             index_of[x] = i
         return []
+
+
+## A better solution with a faster runtime
+
+from typing import List
+
+class Solution:
+    __slots__ = ()
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        y = 1 << ((x.bit_length() + 1) // 2)
+        while y * y > x:
+            y = (y + x // y) // 2
+        return y
